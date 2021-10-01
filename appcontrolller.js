2eb1080575437cfcd1e7342898b7b86dd39f7e72
62030099kmitl <62030099@kmitl.ac.th>
2021-10-01 22:24:34 +0700

    Initial commit

---
+++ appcontrolller.js
@@ -0,0 +1,14 @@
+var myApp =  angular.module('myApp',[]); 
+
+myApp.controller('myAppController',['$scope', '$http', function ($scope, $http, myAppService) { 
+
+    $scope.hu = [];
+
+    $scope.init = function() { 
+
+        $http.get('http://localhost:9000/mainchart').then(function(result) { $scope.hu = result.data; });
+    } 
+    
+    $scope.init(); 
+
+}]);
